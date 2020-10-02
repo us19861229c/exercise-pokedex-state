b49128be7e0974c9fc1f27208e2b3b2234dfc861
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-02 20:28:41 -0300

    Cria botoes do tipo Fire and Psychic

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,35 +2,65 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
-    constructor() {
-        super();
+  constructor(props) {
+    super(props);
 
-        this.handleNextPokemon = this.handleNextPokemon.bind(this);
+    this.handleNextPokemon = this.handleNextPokemon.bind(this);
+    this.handlePokemonType = this.handlePokemonType.bind(this);
 
-        this.state = {
-            pokemonDisplayed: 0,
-        }
-    }
+    const { pokemons } = this.props;
 
-    handleNextPokemon(pokemonList) { 
-        this.setState((state) => ({
-            pokemonDisplayed: (
-                (pokemonList.length === this.state.pokemonDisplayed + 1) 
-                    ? 0 : state.pokemonDisplayed + 1
-                ),
-        }))        
-    }
+    this.state = {
+      pokemonDisplayed: 0,
+      allPokemons: pokemons,
+    };
+  }
 
-    render() {
-        const { pokemons } = this.props;
-        const { pokemonDisplayed } = this.state;
-        return (
-            <div className="pokedex">
-                <Pokemon key={pokemons[pokemonDisplayed].id} pokemon={pokemons[pokemonDisplayed]} />
-                <button onClick={() => this.handleNextPokemon(pokemons)}>Próximo</button>
-            </div>
-        );
-    }
+  handleNextPokemon(pokemonList) {
+    this.setState((state) => ({
+      pokemonDisplayed: (
+        (pokemonList.length === this.state.pokemonDisplayed + 1)
+          ? 0 : state.pokemonDisplayed + 1
+      ),
+    }))
+  }
+
+  handlePokemonType(target) {
+    const { pokemons } = this.props;
+    const pokemonType = target.innerText;
+    const myPokemons = pokemons.filter((pokemon) => pokemon.type === pokemonType);
+    this.setState({allPokemons: myPokemons});
+  }
+
+  render() {
+    const { pokemonDisplayed, allPokemons } = this.state;
+    return (
+      <div className="pokedex">
+        <Pokemon
+          key={allPokemons[pokemonDisplayed].id}
+          pokemon={allPokemons[pokemonDisplayed]}
+        />
+        <div className="buttons-pokemon">
+          <button
+            onClick={(event) => this.handlePokemonType(event.target)}
+          >
+            Fire
+          </button>
+          <button
+            onClick={(event) => this.handlePokemonType(event.target)}
+          >
+            Psychic
+          </button>
+          <br />
+          <button
+            onClick={() => this.handleNextPokemon(allPokemons)}
+          >
+            Próximo
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Pokedex;
