325e322a7200be0f5754d898f34613102dda3158
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-02 18:12:17 -0300

    cria funcionalidade de exibir um pokemon por vez

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,32 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor() {
+        super();
+
+        this.handleNextPokemon = this.handleNextPokemon.bind(this);
+
+        this.state = {
+            pokemonDisplayed: 0,
+        }
+    }
+
+    handleNextPokemon(pokemonList) { 
+        this.setState((state) => ({
+            pokemonDisplayed: (
+                (pokemonList.length === this.state.pokemonDisplayed + 1) 
+                    ? 0 : state.pokemonDisplayed + 1
+                ),
+        }))        
+    }
+
     render() {
+        const { pokemons } = this.props;
+        const { pokemonDisplayed } = this.state;
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon key={pokemons[pokemonDisplayed].id} pokemon={pokemons[pokemonDisplayed]} />
+                <button onClick={() => this.handleNextPokemon(pokemons)}>Próximo</button>
             </div>
         );
     }
